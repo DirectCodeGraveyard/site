f109491b54c491e6754249b405940d7d9d904e5d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-02-01 22:15:40 -0500

    Such Nice SITE! WOW!

---
+++ SimpleCI/site.js
@@ -0,0 +1,3 @@
+$("#learn-more").click(function () {
+   bootbox.alert("<p><b class=\"learn-more-alert\">SimpleCI is a CI built to provide a simple interface but still have all the good features! These include, IRC Support, Easy Plugins, Easy-to-use interface, Security support, and more!</b></p>");
+});
