e47bc6922d1c05fa0479cdbb87f110dff7518fb8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-02-13 23:31:41 -0500

    Nice tweaks to the SimpleCI Site

--- SimpleCI/site.js
+++ SimpleCI/site.js
@@ -2,3 +2,12 @@ $("#learn-more").click(function() {
     bootbox.alert('<p><b class="learn-more-alert">SimpleCI is a CI built to provide a simple interface but still have all the good features! These include, IRC Support, Easy Plugins, Easy-to-use interface, Security support, and more!</b></p>');
 });
 
+var contribList = $("#contributor-list");
+
+[
+    "Kenneth Endfinger",
+    "samrg472",
+    "Logan Gorence"
+].forEach(function (contributor) {
+    contribList.append('<tr><td>' + contributor + '</td></tr>');
+});
