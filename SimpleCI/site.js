e310ccb8d80c4b01f7455cc3fdefe471df69a6fa
Kenneth Endfinger <kaendfinger@gmail.com>
2014-02-13 22:57:42 -0500

    Use single-quote string for learn more

--- SimpleCI/site.js
+++ SimpleCI/site.js
@@ -1,3 +1,4 @@
 $("#learn-more").click(function() {
-    bootbox.alert("<p><b class=\"learn-more-alert\">SimpleCI is a CI built to provide a simple interface but still have all the good features! These include, IRC Support, Easy Plugins, Easy-to-use interface, Security support, and more!</b></p>");
+    bootbox.alert('<p><b class="learn-more-alert">SimpleCI is a CI built to provide a simple interface but still have all the good features! These include, IRC Support, Easy Plugins, Easy-to-use interface, Security support, and more!</b></p>');
 });
+
