0af624ff1640e8e94667480849ffa082e0a31d29
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-02 18:59:19 -0400

    Updated Travis YAML

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,79 @@
 var cssFiles = [
-    'DirectBuild/core.css',
-    'developers/core.css',
-    'css/core.css'
+   'DirectBuild/core.css',
+   'developers/core.css',
+   'css/core.css'
 ];
 
 var jsFiles = [
-    'DirectBuild/site.js',
-    'developers/site.js',
-    'js/site.js'
+   'DirectBuild/site.js',
+   'developers/site.js',
+   'js/site.js'
 ];
 
 /* global module:false */
 module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        // Task configuration.
-        jshint: {
+   // Project configuration.
+   grunt.initConfig({
+      // Task configuration.
+      jshint: {
+         options: {
+            curly: true,
+            eqeqeq: true,
+            immed: true,
+            newcap: true,
+            noarg: true,
+            sub: true,
+            undef: true,
+            unused: true,
+            boss: true,
+            eqnull: true,
+            browser: true,
+            freeze: true,
+            globals: {
+               "$": false,
+               "console": false,
+               "jQuery": false,
+               "require": false,
+               "bootbox": false
+            }
+         },
+         gruntfile: {
+            src: 'Gruntfile.js'
+         },
+         site: {
+            src: jsFiles
+         }
+      },
+      watch: {
+         gruntfile: {
+            files: '<%= jshint.gruntfile.src %>',
+            tasks: ['jshint:gruntfile']
+         },
+         lib_test: {
+            files: '**/**',
+            tasks: ['default']
+         }
+      },
+      csslint: {
+         strict: {
             options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                freeze: true,
-                globals: {
-                    "$": false,
-                    "console": false,
-                    "jQuery": false,
-                    "require": false,
-                    "bootbox": false
-                }
+               import: 0,
+               ids: false
             },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            site: {
-                src: jsFiles
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '**/**',
-                tasks: ['default']
-            }
-        },
-        csslint: {
-            strict: {
-                options: {
-                    import: 0,
-                    ids: false
-                },
-                src: cssFiles
-            }
-        },
-        jsbeautifier: {
-            files: jsFiles,
-            options: {}
-        }
-    });
+            src: cssFiles
+         }
+      },
+      jsbeautifier: {
+         files: jsFiles,
+         options: {}
+      }
+   });
 
-    require('load-grunt-tasks')(grunt);
+   require('load-grunt-tasks')(grunt);
 
-    // Default task.
-    grunt.registerTask('default', ['jsbeautifier', 'jshint', 'csslint']);
-    // Travis CI Task
-    grunt.registerTask('travis', ['jshint', 'csslint']);
+   // Default task.
+   grunt.registerTask('default', ['jsbeautifier', 'jshint', 'csslint']);
+   // Travis CI Task
+   grunt.registerTask('travis', ['jshint', 'csslint']);
+   grunt.registerTask('test', ['jshint', 'csslint']);
 };
