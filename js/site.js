f85e65ee65a221943ff68072a739c9a140c7d7ab
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-05 15:34:24 -0400

    More Bug Fixes and Cleanup

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
-var directcode = {};
+var directcode = {
+    _data_: {
+        logger: {
+            silent: false,
+            debug: false
+        }
+    }
+};
 
 /**
  * A Simple Function to get/change the title using jQuery. 
@@ -10,3 +17,19 @@ directcode.title = function (val) {
         $("head").find("title").text(val);
     }
 };
+
+directcode.debug = function (line) {
+    if (directcode._data_.logger.debug) {
+        console.log("[DirectCode][DEBUG] " + line);
+    }
+};
+
+directcode.log = function (line) {
+    if (!directcode._data_.logger.silent) {
+        console.log("[DirectCode] " + line);
+    }
+};
+
+$(document).ready(function () {
+   directcode.log("Page Ready"); 
+});
