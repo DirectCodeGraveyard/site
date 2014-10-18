1096789bd34bfcd6c2603eaa1f40288d2e6bd080
Kenneth Endfinger <kaendfinger@gmail.com>
2014-10-18 14:26:53 -0400

    :D

---
+++ web/lib/card.dart
@@ -0,0 +1,11 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+
+@CustomTag('fancy-card')
+class FancyCard extends PolymerElement {
+  FancyCard.created() : super.created();
+  
+  @override
+  attached() {
+  }
+}
